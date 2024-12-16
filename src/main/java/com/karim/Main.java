6c427d311db76c3f3213e21e8d1b747e809d1aa5
karim <karimloco4@gmail.com>
2024-12-16 14:28:08 +0100

    second problem

--- src/main/java/com/karim/Main.java
+++ src/main/java/com/karim/Main.java
@@ -3,7 +3,7 @@ package com.karim;
 public class Main {
     public static void main(String[] args) {
         Integer[] list = new Integer[]{1, 2, 3, 4, 5};
-        System.out.println(returnLast(list));
+        System.out.println(returnLastTwo(list));
     }
 
 
@@ -22,5 +22,22 @@ public class Main {
         return recursiveReturnLast(arr, index + 1);
     }
 
+
+    // Problem 2
+    record Pair<T>(T first, T second) { }
+
+    public static <T> Pair<T> returnLastTwo(T[] arr) {
+        if (arr == null || arr.length < 2) {
+            return null;
+        }
+        return recursiveReturnLastTwo(arr, 0);
+    }
+
+    private static <T> Pair<T> recursiveReturnLastTwo(T[] arr, int index) {
+        if (index == arr.length - 2) {
+            return new Pair<>(arr[index], arr[index + 1]);
+        }
+        return recursiveReturnLastTwo(arr, index + 1);
+    }
 
 }
